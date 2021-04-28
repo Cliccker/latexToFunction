09408d91e8546060b9f7d2ecb377ae4c50239963
Hannnk <52920981+Cliccker@users.noreply.github.com>
2021-04-28 22:01:48 +0800

    add

--- LatexToFunctions.py
+++ LatexToFunctions.py
@@ -12,10 +12,8 @@ from pyparsing import *
 one_original = "\varepsilon_{1}=\frac{\sigma_{1}}{E_{\mathrm{y}}}+\gamma_{\mathrm{1}}+\gamma_{2}"
 one_all = "\\varepsilon_{1}=\\frac{\sigma_{1}}{E_{y}}+\gamma_{1}+\gamma_{2}"
 
-
 two_original = "A_{1}=\frac{\sigma_{ys}\left(1+\varepsilon_{ys}\right)}{\left(\ln \left[1+\varepsilon_{ys}\right]\right)^{m_{1}}}"
 two_all = "A_{1}=\\frac{\sigma_{ys}(1+\\varepsilon_{ys})}{(\ln(1+\\varepsilon_{ys}))^{m_{1}}} "
-
 
 paraWithFoot = Word(alphas) + "_{" + Word(alphanums) + "}"  # 有脚标参数
 parameter = Word(alphanums)  # 无脚标参数
@@ -24,5 +22,8 @@ plus = Literal("+")  # 加
 leftBrackets = Literal("(")  # 左括号
 rightBrackets = Literal(")")  # 右括号
 formula = paraWithFoot | parameter | equal | plus | leftBrackets | rightBrackets
+frac = Word("\\frac{") + formula + "}" + "{" + formula + "}"
 formulaTokens = formula.searchString(one_all)  # 寻找所有参数
+fracTokens = frac.searchString(one_all)  # 分数的两个参数
 print(formulaTokens)
+print(fracTokens)
